5c3f1e3663bda0a4c9c224241ca69987d93e38fa
Miguel Sarenas <migui0323@gmail.com>
2022-11-28 17:44:48 -0500

    finish unicafe step 3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,33 +4,78 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-const Display = ({ text, number }) => (
-  <p>
-    {text} {number}
-  </p>
-);
+const Display = ({ text, number, symbol }) => {
+  if (symbol) {
+    return (
+      <p>
+        {text} {number} {symbol}
+      </p>
+    );
+  }
+  return (
+    <p>
+      {text} {number}
+    </p>
+  );
+};
+
+const Statistics = ({ feedback }) => {
+  const calculations = {
+    getAllStatistics: () => feedback.good + feedback.neutral + feedback.bad,
+    getAverage: () =>
+      (feedback.good - feedback.bad) /
+      (feedback.good + feedback.neutral + feedback.bad),
+  };
+  return (
+    <>
+      <h2>statistics</h2>
+      <Display text="good" number={feedback.good} />
+      <Display text="neutral" number={feedback.neutral} />
+      <Display text="bad" number={feedback.bad} />
+      <Display text="all" number={calculations.getAllStatistics()} />
+      <Display text="average" number={calculations.getAverage()} />
+      <Display
+        text="positive"
+        number={(feedback.good / calculations.getAllStatistics()) * 100}
+        symbol={"%"}
+      />
+    </>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+
+  // TODO: turn all the useState variables into one object and refactor the calculated variables
+  // TODO: create one object for all the state and pass it as props to the child components
+  // TODO: find a way to pass in all calculations as a prop to the statistics object
+
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
 
   const incrementGood = () => {
-    setGood(good + 1);
+    setFeedback({
+      ...feedback,
+      good: feedback.good + 1,
+    });
   };
 
   const incrementNeutral = () => {
-    setNeutral(neutral + 1);
+    setFeedback({
+      ...feedback,
+      neutral: feedback.neutral + 1,
+    });
   };
 
   const incrementBad = () => {
-    setBad(bad + 1);
+    setFeedback({
+      ...feedback,
+      bad: feedback.bad + 1,
+    });
   };
-
-  const allStatistics = good + neutral + bad;
-
-  const average = (good - bad) / allStatistics;
 
   return (
     <>
@@ -38,14 +83,7 @@ const App = () => {
       <Button text="good" handleClick={incrementGood} />
       <Button text="neutral" handleClick={incrementNeutral} />
       <Button text="bad" handleClick={incrementBad} />
-
-      <h2>statistics</h2>
-      <Display text="good" number={good} />
-      <Display text="neutral" number={neutral} />
-      <Display text="bad" number={bad} />
-      <Display text="all" number={allStatistics} />
-      <Display text="average" number={average} />
-      <Display text="positive" number={good / allStatistics} />
+      <Statistics feedback={feedback} />
     </>
   );
 };
