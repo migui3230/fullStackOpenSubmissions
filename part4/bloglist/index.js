a90493818bd23f67a15c6c44447906a8f796963d
Miguel Sarenas <migui0323@gmail.com>
2023-01-08 22:15:50 -0500

    finish blog list step 2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -6,43 +6,25 @@ TODO:
 */
 
 require("dotenv").config();
-const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-mongoose.set("strictQuery", true);
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const blogsRouter = require("./controllers/blogs");
 
 const mongoUrl = process.env.MONGODB_URL;
-mongoose.connect(mongoUrl);
+mongoose.connect(mongoUrl).then(() => {
+  console.log("connected to db");
+});
 
 app.use(cors());
 app.use(express.json());
+app.use(blogsRouter);
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = 3003;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
