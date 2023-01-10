fd057bd1a4356a3447d243bba0deb610136e082c
Miguel Sarenas <migui0323@gmail.com>
2023-01-10 09:07:22 -0500

    finish helper functions and unit tests step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1;
 };
 
+// TODO: change this function to parse through the array of blog objects instead
+const totalLike = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  }
+
+  return blogs.reduce((total, blog) => total + blog.likes, 0);
+};
+
 module.exports = {
   dummy,
+  totalLike,
 };
